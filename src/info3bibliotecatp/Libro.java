c9d5ae97ed8a6db3e797b5a3e5cb74b7f68abb97
rutharamio <rutharamio@gmail.com>
2025-06-25 22:19:51 -0300

    Primer commit

---
+++ src/info3bibliotecatp/Libro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package info3bibliotecatp;
+
+/**
+ *
+ * @author MSI
+ */
+public class Libro {
+     private String titulo;
+    private String autor;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + autor;
+    }
+}
+
